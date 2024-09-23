5d0d737835538175c62619cd7319d4c4f599c7ce
Florence Murkfulsky <162458889+franklin13512@users.noreply.github.com>
2024-09-23 14:56:13 +0800

    栈数组

---
+++ MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsProject
+{
+    internal class MyStack<T>
+    {
+        private MyList1<T> TheList;
+
+        public MyStack()
+        {
+            TheList = new MyList1<T>();
+        }
+
+        public int Number
+        {
+            get { return TheList.Number;  }
+        }
+
+        public T Peek
+        {
+            get { return TheList[Number - 1]; }
+        }
+
+        public T Pop()
+        {
+            return TheList.RemoveAtLast();
+        }
+
+        public void Push(T NewData)
+        {
+            TheList.AddAtLast(NewData);
+        }
+
+        public bool IsEmpty()
+        {
+            if (TheList.Number == 0)
+            {
+                return true;
+            }else { return false; }
+        }
+
+        public override string ToString()
+        {
+            return "The Stack: " + TheList.ToString() + " Top ";
+        }
+    }
+}
